5c4b49b1fc29fbc6d433beab6047e290748cab66
Ophir Bushinsky <ophirbushi@gmail.com>
2018-12-21 20:55:05 +0200

    wip

--- app/routes/user-details.js
+++ app/routes/user-details.js
@@ -1,46 +1,22 @@
+const cookie = require('cookie');
 const jwt = require('jsonwebtoken');
 const secret = require('../secret');
 const {
     UsersService
 } = require('../lib/users.service');
 
-module.exports = (req, res) => {
-    const cookie = req.headers.cookie;
-    if (cookie) {
-        const cookies = cookie.split(';');
-
-        const keyValues = cookies.map(c => {
-            c = c.trim();
-            const split = c.split('=');
-            if (split) {
-                return {
-                    key: split[0],
-                    value: split[1]
-                };
-            }
-            return {
-                key: -1,
-                value: -1
-            };
-        });
-        const accessToken = keyValues.find(pair => pair.key === 'access-token');
-
-        if (accessToken) {
-            try {
-                const decoded = jwt.verify(accessToken.value, secret);
-                const userName = decoded['userName'];
-                new UsersService().getUser(userName)
-                    .then(details => {
-                        if (details) res.send(details);
-                        else res.sendStatus(401);
-                    });
-            } catch (err) {
-                res.sendStatus(500);
-            }
-        } else {
-            return res.sendStatus(401);
-        }
-    } else {
-        return res.sendStatus(401);
+module.exports = async (req, res) => {
+    const cookies = cookie.parse(req.headers.cookie);
+    const accessToken = cookies['access-token'];
+    if (!accessToken) return res.sendStatus(401);
+    try {
+        const decoded = jwt.verify(accessToken, secret);
+        const userName = decoded['userName'];
+        const user = await new UsersService().getUser(userName);
+        if (!user) return res.sendStatus(401);
+        res.send(user);
+    } catch (error) {
+        console.error('error: ', error);
+        res.sendStatus(500);
     }
 };
